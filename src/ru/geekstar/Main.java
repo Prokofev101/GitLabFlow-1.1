2a5a43da9dd23d1458cd6307eb1961bcc610aac0
Valery Prokofev <vvvpvv1991@gmail.com>
2022-07-08 13:07:10 +0400

    add description perproduction

--- src/ru/geekstar/Main.java
+++ src/ru/geekstar/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// main - Ветка предназначена для разработки и может быть не стабильной
         // production - Ветка для выпуска новой версии продукта
+        // preproduction - Ветка для тестирования перед внедрением
     }
 }
